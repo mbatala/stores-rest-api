572300dc0f80f8a4b04c4c06fac0db37e298f54a
mbatala <mihael.batala@gmail.com>
2019-02-23 12:27:40 +0100

    First commit, added REST API to acces items, stores, and user authentication.

---
+++ resources/item.py
@@ -0,0 +1,74 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type = float,
+        required = True,
+        help = "This filed cannot be left blank!"
+    )
+    parser.add_argument('store_id',
+        type = int,
+        required = True,
+        help = "Every item needs a store id."
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': "Item with name '{}' not found!".format(name)}, 404
+
+
+    @jwt_required()
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400 # Bad request status code
+
+        data = Item.parser.parse_args()
+
+        #item = ItemModel(name, data['price'], data['store_id'])
+        item = ItemModel(name, **data)
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item with name '{}'.".format(name)}, 500 # Internal Server error
+
+        return item.json(), 201 # http status code for successful creating an item
+
+
+    @jwt_required()
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {"message": "An item with name '{}' deleted".format(name)}, 200
+        return {'message': "An item with name '{}' doesn't exist.".format(name)}, 400
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            #item = ItemModel(name, data['price'], data['store_id'])
+            item = ItemModel(name, **data)
+        else:
+            item.price = data['price']
+
+        item.save_to_db()
+
+        return item.json(), 200
+
+
+class ItemList(Resource):
+    @jwt_required()
+    def get(self):
+        return {'items': [item.json() for item in ItemModel.query.all()]}, 200
+        #return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
---
+++ resources/store.py
@@ -0,0 +1,38 @@
+from flask_restful import Resource
+from models.store import StoreModel
+from flask_jwt import jwt_required
+
+class Store(Resource):
+    @jwt_required()
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json() # default status is 200 and it is not necessary to be explicitly put
+        return {'message': "Store '{}' not found!".format(name)}, 404
+
+    @jwt_required()
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "A store with name '{}' already exists!".format(name)}, 400
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message": "An error occurred while creating the store '{}'.".format(name)}, 500
+
+        return store.json(), 201
+
+    @jwt_required()
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+            return {"message": "Store '{}' deleted".format(name)}
+        else:
+            return {"message": "Store '{}' does not exists!".format(name)}, 400
+
+
+class StoreList(Resource):
+    @jwt_required()
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
