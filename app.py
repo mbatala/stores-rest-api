cfde779783fe22dcda637562dcf097ca119f9ed3
mbatala <mihael.batala@gmail.com>
2019-02-23 22:44:17 +0100

    try again

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -13,12 +14,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-"""
+db.init_app(app)
+
 @app.before_first_request
 def create_tables():
     db.create_all()
-"""
-
+    
 jwt = JWT(app, authenticate, identity) # /auth
 
 api.add_resource(Store, '/store/<string:name>')
@@ -28,7 +29,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+    #from db import db
+    #db.init_app(app)
     #app.run(port=5000, debug=True)
     app.run(debug = True)
